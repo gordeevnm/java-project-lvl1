9802ea7dc67f230a28277c8d71b2219b7b71e1c6
gordeevnm <gordeevnm@gmail.com>
2021-04-18 16:59:01 +0300

    fix PrimeGame.isPrime()

--- src/main/java/hexlet/code/games/PrimeGame.java
+++ src/main/java/hexlet/code/games/PrimeGame.java
@@ -107,7 +107,7 @@ public final class PrimeGame {
         if (num < 2) {
             return false;
         }
-        for (int i = 2; i < Math.sqrt(num); i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
             }
